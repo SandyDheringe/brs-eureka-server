976e8a6897d7fac53c9049df625dba22539728db
sandeep <sandeep.ariseup@gmail.com>
2023-08-17 17:01:02 +0530

    Initial commit

---
+++ src/main/java/com/brseurekaserver/BrsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.brseurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrsEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrsEurekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brseurekaserver/BrsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brseurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrsEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

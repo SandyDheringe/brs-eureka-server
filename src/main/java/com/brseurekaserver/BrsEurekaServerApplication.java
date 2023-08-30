fcd363a226616fb7e986655a4e763839d812f96b
sandeep <sandeep.ariseup@gmail.com>
2023-08-31 01:13:57 +0530

    docker changes

--- src/main/java/com/brseurekaserver/BrsEurekaServerApplication.java
+++ src/main/java/com/brseurekaserver/BrsEurekaServerApplication.java
@@ -2,6 +2,7 @@ package com.brseurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
@@ -10,7 +11,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class BrsEurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BrsEurekaServerApplication.class, args);
+//        SpringApplication.run(BrsEurekaServerApplication.class, args);
+
+        new SpringApplicationBuilder()
+                .profiles("dev")
+                .sources(BrsEurekaServerApplication.class)
+                .run(args);
     }
 
 }
